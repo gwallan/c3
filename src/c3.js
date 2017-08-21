9f8d8dccd0f7879baa27df0d256f8f896753f51a
gongwei <gongwei@adsage.com>
2017-08-21 11:36:55 +0800

    fixed

--- src/c3.js
+++ src/c3.js
@@ -2229,7 +2229,7 @@
                 if ($$.isAvailableEventRect()) { return; }
                 if($$.isTouchDevice){return;}
 
-                $$.unexpandBars && $$.hideXGridFocus();
+                $$.hideXGridFocus && $$.hideXGridFocus();
                 $$.hideTooltip && $$.hideTooltip();
                 $$.unexpandCircles && $$.unexpandCircles(index);
                 $$.unexpandBars && $$.unexpandBars();
